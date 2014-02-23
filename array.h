4da9f1f358eaeb16a35fcaf2999cd2b6d0c92738
Roman <roma.turkin@yahoo.com>
2014-02-23 11:06:11 +0400

    initial project version

---
+++ array.h
@@ -0,0 +1,16 @@
+#define ARRAY void *
+#define DATA void *
+#define INDEX long
+
+
+ARRAY create_array();
+//NULL in case of allocating problems
+
+int insert(ARRAY, INDEX, DATA);
+//if DATA = NULL it equals to deleting element, 0 in case of success
+
+DATA get(ARRAY, INDEX);
+//returns NULL in case of failure
+
+int destroy_array(ARRAY);
+//-1 if there is no such array

51ca2e07441736c61d22dd4e355ec2f68a014f31
Igor Stepanov <gavno23@gmail.com>
2014-03-03 01:31:20 +0400

    add stepanov

---
+++ stepanov/array.c
@@ -0,0 +1,22 @@
+#include "../array.h"
+#include <stdlib.h>
+
+struct node {
+	INDEX index;
+	DATA data;
+};
+
+ARRAY create_array() {
+}
+
+int insert(ARRAY array, INDEX indexIn, DATA dataIn) {
+	
+}
+
+DATA get(ARRAY array, INDEX index) {
+
+}
+
+int destroy_array(ARRAY array) {
+
+}

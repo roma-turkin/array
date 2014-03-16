266c4f5012e23cef02bfeaab19be7df724e5a743
Roman <roma.turkin@yahoo.com>
2014-03-16 23:09:48 +0400

    On branch master 	modified:   coverage.sh 	modified:   test.c

--- turkin/coverage.sh
+++ turkin/coverage.sh
@@ -2,8 +2,10 @@
 
 # gcc -fprofile-arcs -ftest-coverage array.c test.c 
 gcc --coverage -fprofile-arcs -ftest-coverage array.c test.c
- ./a.out
-lcov --capture --directory /home/roman/programming/array/turkin --output-file coverage.info
+./a.out
+DIRECTORY=$(pwd)
+lcov --capture --directory $DIRECTORY --output-file coverage.info
 genhtml coverage.info --output-directory out
 rm a.out
 rm *.gc*
+rm *.info

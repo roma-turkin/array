68147d479dc9b4da03459cfa2b6ce88c51b39878
roma-turkin <roma.turkin@yahoo.com>
2014-03-19 19:49:43 +0400

    Changes to be committed: 	modified:   runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,43 +1,41 @@
 #! /bin/bash
 # gcc -fprofile-arcs -ftest-coverage svikhnushina/array.c turkin/test.c 
-rm error_log
-rm output_log
-gcc turkin/array.c turkin/test.c -o tt 	   		     2>error_log
-./tt                                                 1>output_log
-RESULT=$?
-if [ "$RESULT" -ne 0 ]
-	then echo "tt failed"
-fi
-rm tt
 
-gcc turkin/array.c svikhnushina/test.c -o tsv        2>error_log
-./tsv                                                1>output_log
-RESULT=$?
-if [ "$RESULT" -ne 0 ]
-	then echo "tsv failed"
-fi
-rm tsv
+names=("turkin" "lysenko" "svikhnushina" "stepanov" "afanasyev")
 
-gcc turkin/array.c stepanov/test.c -o tst 			 2>error_log
-./tst                                                1>output_log
-RESULT=$?
-if [ "$RESULT" -ne 0 ]
-	then echo "tst failed"
-fi
-rm tst
+#ARRAY_FOLDER_NAME="turkin"
+#TEST_FOLDER_NAME="turkin"
+for ARRAY_FOLDER_NAME in ${names[*]}
+do
+	for TEST_FOLDER_NAME in ${names[*]}
+	do
+		gcc $ARRAY_FOLDER_NAME/array.c $TEST_FOLDER_NAME/test.c 	   		     	 2>error_log
+		./a.out     					                                             1>output_log
+		RESULT=$?
+		COMPILATION_ERRORS="No errors occured"
+		if [ $(stat -c %s error_log) -ne 0 ]
+		then 
+			COMPILATION_ERRORS="ERROR! Recompile to check"
+		fi
+		echo $ARRAY_FOLDER_NAME $TEST_FOLDER_NAME $RESULT $COMPILATION_ERRORS
+	done
+done
 
-gcc turkin/array.c afanasyev/test.c -o ta			 2>error_log
-./ta                                                 1>output_log
-RESULT=$?
-if [ "$RESULT" -ne 0 ]
-	then echo "ta failed"
-fi
-rm ta
-
-gcc turkin/array.c lysenko/test.c -o tl              2>error_log
-./tl                                                 1>output_log
-RESULT=$?
-if [ "$RESULT" -ne 0 ]
-	then echo "tl failed"
-fi
-rm tl
+#ARRAY_FOLDER_NAME="afanasyev"
+#TEST_FOLDER_NAME="afanasyev"
+#gcc $ARRAY_FOLDER_NAME/array.c $TEST_FOLDER_NAME/test.c 	   		     	 2>error_log
+#./a.out     					                                             1>output_log
+#RESULT=$?
+#COMPILATION_ERRORS="No errors occured"
+#if [ $(stat -c %s error_log) -ne 0 ]
+#then 
+#	COMPILATION_ERRORS="ERROR! Recompile to check"
+#fi
+#echo $ARRAY_FOLDER_NAME $TEST_FOLDER_NAME $RESULT $COMPILATION_ERRORS
+#gcc turkin/array.c svikhnushina/test.c -o tsv        2>error_log
+#./tsv                                                1>output_log
+#RESULT=$?
+#if [ "$RESULT" -ne 0 ]
+#	then echo "tsv failed"
+#fi
+#rm tsv

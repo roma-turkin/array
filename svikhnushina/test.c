8291460860ccf3d88a6fa83f95f33b7e2ce9dc47
roma-turkin <roma.turkin@yahoo.com>
2014-03-19 23:29:05 +0400

    On branch master  Changes to be committed: 	modified:   test.c

--- svikhnushina/test.c
+++ svikhnushina/test.c
@@ -4,6 +4,7 @@
 #include <time.h>
 
 #define COUNT   1
+#define ERROR {printf("Error in %s on line %d\n",__FILE__,__LINE__); res = -1;};
 
 /* sort array */
 void sort_array(ARRAY array, int size)
@@ -16,12 +17,14 @@ void sort_array(ARRAY array, int size)
             val2 = (int) get(array, j);
             if ( val1 >= val2 ) break;
             if ( insert(array, j, (void *) val1) ) {
-                printf("insert(%i, %i): failed\n", j, val1);
-                exit(-1);
+                /*printf("insert(%i, %i): failed\n", j, val1);
+                exit(-1);*/
+                ERROR
             }
             if ( insert(array, j + 1, (void *) val2) ) {
-                printf("insert(%i, %i): failed\n", j + 1, val2);
-                exit(-1);
+                /*printf("insert(%i, %i): failed\n", j + 1, val2);
+                exit(-1);*/
+                ERROR
             }
         }
     }
@@ -29,6 +32,7 @@ void sort_array(ARRAY array, int size)
 
 int main()
 {
+	int res = 0;
     ARRAY array1 = create_array();
     ARRAY array2 = create_array();
     int i, size, cleared, val1, val2, cnt;
@@ -36,12 +40,14 @@ int main()
 
     /* create array & check */
     if ( !array1 ) {
-        printf("create_array(): failed\n");
-        exit(-1);
+        /*printf("create_array(): failed\n");
+        exit(-1);*/
+        ERROR
     }
     if ( !array2 ) {
-        printf("create_array(): failed\n");
-        exit(-1);
+        /*printf("create_array(): failed\n");
+        exit(-1);*/
+        ERROR
     }
 
 
@@ -59,16 +65,18 @@ int main()
             size = val2;
             cleared = val1;
         }
-        printf("Test %i: origin size=%i, elements to be removed =%i", cnt+1, size, cleared);
+        //printf("Test %i: origin size=%i, elements to be removed =%i", cnt+1, size, cleared);
         for (i = 0; i < size; i++) {
             data = (void *) (rand() + 1);
             if ( insert(array1, i, data) ) {
-                printf("insert(array1, %i, %i): failed\n", i, (int) data);
-                exit(-1);
+              /*  printf("insert(array1, %i, %i): failed\n", i, (int) data);
+                exit(-1); */
+                ERROR
             }
             if ( insert(array2, size - i - 1, data) ) {
-                printf("insert(array2, %i, %i): failed\n", i, (int) data);
-                exit(-1);
+                /*printf("insert(array2, %i, %i): failed\n", i, (int) data);
+                exit(-1);*/
+                ERROR
             }
         }
 
@@ -78,12 +86,15 @@ int main()
         /* part clear */
         for (i = size - cleared; i < size; i++) {
             if ( insert(array1, i, NULL) ) {
-                printf("insert(array1, %i, NULL): failed\n", i);
+                /*printf("insert(array1, %i, NULL): failed\n", i);
                 exit(-1);
+                */
+                ERROR
             }
             if ( insert(array2, i, NULL) ) {
-                printf("insert(array2, %i, NULL): failed\n", i);
-                exit(-1);
+                /*printf("insert(array2, %i, NULL): failed\n", i);
+                exit(-1);*/
+                ERROR
             }
         }
         size -= cleared;
@@ -92,43 +103,50 @@ int main()
         for (i = 0; i < size; i++) {
             val1 = (int) get(array1, i);
             if ( !val1 ) {
-                printf("get(array1, %i): failed\n", i);
-                exit(-1);
+                /*printf("get(array1, %i): failed\n", i);
+                exit(-1);*/
+                ERROR
             }
             val2 = (int) get(array2, i);
             if ( !val2 ) {
-                printf("get(array2, %i): failed\n", i);
-                exit(-1);
+                /*printf("get(array2, %i): failed\n", i);
+                exit(-1);*/
+                ERROR
             }
             if ( val1 != val2 ) {
-                printf("array1[%i]=%i <> array2[%i]=%i\n", i, val1, i, val2);
-                exit(-1);
+                /*printf("array1[%i]=%i <> array2[%i]=%i\n", i, val1, i, val2);
+                exit(-1);*/
+                ERROR
             }
         }
 
         /* full clear */
         for (i = 0; i < size; i++) {
             if ( insert(array1, i, NULL) ) {
-                printf("insert(array1, %i, NULL): failed\n", i);
-                exit(-1);
+                /*printf("insert(array1, %i, NULL): failed\n", i);
+                exit(-1);*/
+                ERROR
             }
             if ( insert(array2, i, NULL) ) {
-                printf("insert(array2, %i, NULL): failed\n", i);
-                exit(-1);
+                /*printf("insert(array2, %i, NULL): failed\n", i);
+                exit(-1);*/
+                ERROR
             }
         }
-        printf("\tsucceed\n");
+        //printf("\tsucceed\n");
     }
 
     /* destroyclear arrays */
     if ( destroy_array(array1) ) {
-        printf("destroy_array1(): failed\n");
-        exit(-1);
+        /*printf("destroy_array1(): failed\n");
+        exit(-1);*/
+        ERROR
     };
     if ( destroy_array(array2) ) {
-        printf("destroy_array2(): failed\n");
-        exit(-1);
+        /*printf("destroy_array2(): failed\n");
+        exit(-1);*/
+        ERROR
     };
 
-    return 0;
+    return res;
 }
